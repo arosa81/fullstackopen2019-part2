5c6e1b57ed995493c7917ddeddf7f9fb5fd0eb38
Alexander Rosa <alex.j.rosa@gmail.com>
2019-10-06 11:17:14 -0600

    2.2: Course contents step7

--- coursecontents/src/components/Course.js
+++ coursecontents/src/components/Course.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { bold } from 'ansi-colors';
 
 const Header = ({ courseName }) => <h1>{courseName}</h1>
 const PartsContent = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+const ExerciseSum = ({ parts }) => {
+  let sum = parts.reduce((accum, part) => accum + part.exercises, 0)
+  return (
+    <p><b>total of {sum} exercises</b></p>
+  )
+}
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header courseName={course.name} />
       <PartsContent parts={course.parts} />
+      <ExerciseSum parts={course.parts} />
     </div>
   )
 }
