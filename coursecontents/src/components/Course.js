b4d802cbeecfa19a29563ff01f96bbe7d508bd05
Alexander Rosa <alex.j.rosa@gmail.com>
2019-10-06 11:20:00 -0600

    2.3*: Course contents step8 reduce func updated

--- coursecontents/src/components/Course.js
+++ coursecontents/src/components/Course.js
@@ -5,9 +5,9 @@ const Header = ({ courseName }) => <h1>{courseName}</h1>
 const PartsContent = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 const ExerciseSum = ({ parts }) => {
-  let sum = parts.reduce((accum, part) => accum + part.exercises, 0)
+  const total = parts.reduce((accum, part) => accum + part.exercises, 0)
   return (
-    <p><b>total of {sum} exercises</b></p>
+    <p><b>total of {total} exercises</b></p>
   )
 }
 
