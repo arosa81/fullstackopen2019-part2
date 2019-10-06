89492cc5e8576c89d691287c2efeddf0740c2204
Alexander Rosa <alex.j.rosa@gmail.com>
2019-10-06 10:40:26 -0600

    refactor:  modulerized PartsContent element

--- coursecontents/src/components/Course.js
+++ coursecontents/src/components/Course.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Header = ({ courseName }) => <h1>{courseName}</h1>
+const PartsContent = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header courseName={course.name} />
-      {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      <PartsContent parts={course.parts} />
     </div>
   )
 }
