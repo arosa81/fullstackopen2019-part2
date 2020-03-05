c90abcbb925c6dcd974809d7c51a6b91fd6f4075
Alexander Rosa <alex.j.rosa@gmail.com>
2020-03-04 21:40:28 -0700

    refactor: eslint prettier

--- coursecontents/src/components/Course.js
+++ coursecontents/src/components/Course.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
-const Header = ({ courseName }) => <h1>{courseName}</h1>
-const PartsContent = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+const PartsContent = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />);
+const Part = ({ part }) => (
+    <p>
+        {part.name} {part.exercises}
+    </p>
+);
 const ExerciseSum = ({ parts }) => {
-  const total = parts.reduce((accum, part) => accum + part.exercises, 0)
-  return (
-    <p><b>total of {total} exercises</b></p>
-  )
-}
+    const total = parts.reduce((accum, part) => accum + part.exercises, 0);
+    return (
+        <p>
+            <b>total of {total} exercises</b>
+        </p>
+    );
+};
 
-const Course = ({ course }) => {
-  return (
+const Course = ({ course }) => (
     <div>
-      <Header courseName={course.name} />
-      <PartsContent parts={course.parts} />
-      <ExerciseSum parts={course.parts} />
+        <Header courseName={course.name} />
+        <PartsContent parts={course.parts} />
+        <ExerciseSum parts={course.parts} />
     </div>
-  )
-}
+);
 
 // Seperate module
 export default Course;
