d5c9842ee3f6148125f01945394027563689a46b
Alexander Rosa <alex.j.rosa@gmail.com>
2020-03-09 20:18:38 -0600

    2.16: Phonebook step8 - comms to DB module

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -3,8 +3,10 @@ export const BASE_URL = 'http://localhost:3001/persons';
 
 const getAll = () => axios.get(BASE_URL).then(response => response.data)
 
+//Create person with person object
 const createPerson = newPerson => axios.post(BASE_URL, newPerson).then(response => response.data)
 
+//Update person using ID
 const updatePerson = (id, newPerson) => axios.put(`${BASE_URL}/${id}`, newPerson).then(response => response.data)
 
 export default { getAll, createPerson, updatePerson };
